a5fb02ca1ba87ffec28ff386b3f6bde412494335
Ken <kchylee@gmail.com>
2017-05-16 01:18:27 +0000

    Finished rating section: Now able to add rating, show rating. Rating function limited to signed in users.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,33 @@
 class ReviewsController < ApplicationController
+
+  before_filter :require_login
+
   def create
-    @product = Product.find_by(id: params[:product_id])
+    @review = Review.new(product_id: params[:product_id], description: params[:review][:description], rating: params[:review][:rating], user_id: session[:user_id])
+    @review.save
+
     if @review.save
-      redirect_to "/"
+      flash[:notice] = "Review Saved!"
+      redirect_to "/products/#{params[:product_id]}"
     else
-      redirect_to "/products/#{@product.id}"
+      redirect_to "/products/#{params[:product_id]}"
     end
+
   end
+
+  def destroy
+    @review = Review.find_by(id: params[:id])
+    @review.destroy
+    redirect_to "/products/#{params[:product_id]}"
+  end
+
+  private
+
+    def require_login
+      if session[:user_id].nil?
+        flash[:error] = "You must be logged in to access this section"
+        redirect_to "/login"
+      end
+    end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,4 @@ class Review < ActiveRecord::Base
   belongs_to :product
 
   validates :product_id, :user_id, :description, :rating, presence: true
-
-
 end
